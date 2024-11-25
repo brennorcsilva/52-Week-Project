d3a5ed35462c33d10392c24a916ef1f652265565
brennorcsilva <brennorcsilva@gmail.com>
2024-11-25 20:56:17 -0300

    Week 1 - Secure Password Generator (Python)

---
+++ Secure password generator/app.py
@@ -0,0 +1,28 @@
+from random import choice
+import string
+
+
+special = [ '!', '@', '#', '$', '%', '&', '*', '_', '<', '>' ]
+specialChar = ''.join(special)
+
+password_size = int(input('Input how many characters do u want in your password:'))
+
+include_uppercase = input('Do you want uppercase? (y/n): ').lower() == 'y'
+include_numbers = input('Do you want numbers? (y/n): ').lower() == 'y'
+include_special = input('Do you want special characters? (y/n): ').lower() == 'y'
+
+chars = string.ascii_lowercase
+
+if include_uppercase:
+    chars += string.ascii_uppercase
+if include_numbers:
+    chars += string.digits
+if include_special:
+    chars += specialChar
+
+secure_password = ''
+
+for i in range(password_size):
+    secure_password += choice(chars)
+
+print(secure_password)
