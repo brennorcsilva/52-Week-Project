bf607923cf81ccce80620a4e670cc8e8de64b523
brennorcsilva <brennorcsilva@gmail.com>
2024-11-25 22:09:53 -0300

    API code

---
+++ Secure password generator/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from password_generator import character_pool, generate_password
+
+app = Flask(__name__)
+
+@app.route('/generate-password', methods = ['POST'])
+def generate_password_api():
+    data = request.get_json()
+
+    password_size = data.get('password_size', 0)
+    include_uppercase = data.get('include_uppercase', False)
+    include_numbers = data.get('include_numbers', False)
+    include_special = data.get('include_special', False)
+
+    if password_size <= 0:
+        return jsonify({'error': 'Password size must be greater than 0!'}), 400
+    
+    chars = character_pool(include_uppercase, include_numbers, include_special)
+
+    if not chars:
+        return jsonify({'error': 'No characther types selected!'}), 400
+    
+    password = generate_password(password_size, chars)
+    return jsonify({'password': password})
+
+if __name__ == '__main__':
+    app.run(debug=True)
