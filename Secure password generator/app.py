777e974adb4df968bad95691a9fbc434919fc9a9
brennorcsilva <brennorcsilva@gmail.com>
2024-11-26 00:55:21 -0300

    Adding Javascript

--- Secure password generator/app.py
+++ Secure password generator/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from password_generator import character_pool, generate_password
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/generate-password', methods = ['POST'])
 def generate_password_api():
