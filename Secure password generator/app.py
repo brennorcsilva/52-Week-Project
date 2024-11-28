c87c69debb00654dd1390c39ed38d608dcf83043
brennorcsilva <brennorcsilva@gmail.com>
2024-11-28 00:25:26 -0300

    Finishing the project

--- Secure password generator/app.py
+++ Secure password generator/app.py
@@ -1,9 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from password_generator import character_pool, generate_password
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/generate-password', methods = ['POST'])
 def generate_password_api():
